86d6ee1b3f5e065e001a7b678a64562266127a5c
Lauren Judge <l.m.judge@hotmail.com>
2019-05-17 11:20:08 +1200

    Limit added

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -9,10 +9,11 @@ import { Todo } from '../models/Todo';
 })
 export class TodoService {
   todosUrl:string = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit = '?_limit=5';
 
   constructor(private http:HttpClient) { }
 
   getTodos():Observable<Todo[]> {
-   return this.http.get<Todo[]>(this.todosUrl);
+   return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
 }
