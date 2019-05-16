77f99b45f371c81de8c753f372bb728aad550bd4
Lauren Judge <l.m.judge@hotmail.com>
2019-05-17 11:16:03 +1200

    added get request to json placeholder

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -14,4 +14,21 @@ export class TodoItemComponent implements OnInit {
   ngOnInit() {
   }
 
+  //Set Dynamic Classes 
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+
+    return classes;
+  }
+
+  onToggle(todo) {
+    todo.completed = !todo.completed;
+  }
+
+  onDelete(todo) {
+    console.log('delete');
+  }
 }
